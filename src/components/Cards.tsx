f85a8c9257d3c6c212acee7b26676988304bce2d
raijinhasarrived <raijinhasarrived@gmail.com>
2023-01-30 16:30:39 +0300

    add search by name

--- src/components/Cards.tsx
+++ src/components/Cards.tsx
@@ -8,9 +8,11 @@ export const Cards = () => {
   const { isLoading, error, data } = useQuery('character list', async () => {
     return await CharactersService.getAll();
   });
+
   if (isLoading) return <p className="h-screen">Loading</p>;
   if (error) return <p className="h-screen">Something went wrong</p>;
   console.log(data?.results);
+
   return (
     <div className="grid xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-3 p-5">
       {data?.results.map((persona, index) => (
