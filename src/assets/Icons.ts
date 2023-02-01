b55423e3904f34668ea18030098464e2972953e9
raijinhasarrived <raijinhasarrived@gmail.com>
2023-02-02 02:00:57 +0300

    add icons with links

---
+++ src/assets/Icons.ts
@@ -0,0 +1,38 @@
+import icon_react from './react.svg';
+import icon_tailwind from './tailwindcss-plain.svg';
+import icon_sass from './sass-original.svg';
+import icon_vite from './vite.svg';
+import icon_typescript from './typescript-original.svg';
+import react_query from './react-query.svg';
+import framer_motion from './framer-motion.svg';
+
+export const Icons = [
+  {
+    icon: icon_react,
+    link: 'https://beta.reactjs.org/',
+  },
+  {
+    icon: icon_typescript,
+    link: 'https://www.typescriptlang.org/',
+  },
+  {
+    icon: icon_tailwind,
+    link: 'https://tailwindcss.com/',
+  },
+  {
+    icon: icon_sass,
+    link: 'https://sass-lang.com/',
+  },
+  {
+    icon: icon_vite,
+    link: 'https://vitejs.dev/',
+  },
+  {
+    icon: react_query,
+    link: 'https://react-query-v3.tanstack.com/',
+  },
+  {
+    icon: framer_motion,
+    link: 'https://www.framer.com/motion/',
+  },
+];
--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -1,8 +1,24 @@
+import { Icons } from '../../assets/Icons';
 import styles from './Footer.module.scss';
+
 export const Footer = () => {
+  const tools = Icons;
   return (
     <footer className={`${styles.footer}`}>
-      <div></div>
+      <div className="flex gap-3 px-3 ">
+        {tools.map((tool, index) => (
+          <a className="flex" href={tool.link} key={index} target="_blank">
+            <img
+              className="grayscale-0 sm:grayscale hover:grayscale-0"
+              src={tool.icon}
+              alt="icon"
+              width={20}
+              height={20}
+              key={index}
+            />
+          </a>
+        ))}
+      </div>
       <div></div>
     </footer>
   );
