106be1143ac0a127fb88b80ccbe4ab85b792b030
raijinhasarrived <raijinhasarrived@gmail.com>
2023-02-02 02:15:06 +0300

    fix icons size

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -5,21 +5,18 @@ export const Footer = () => {
   const tools = Icons;
   return (
     <footer className={`${styles.footer}`}>
-      <div className="flex gap-3 px-3 ">
-        {tools.map((tool, index) => (
-          <a className="flex" href={tool.link} key={index} target="_blank">
-            <img
-              className="grayscale-0 sm:grayscale hover:grayscale-0"
-              src={tool.icon}
-              alt="icon"
-              width={20}
-              height={20}
-              key={index}
-            />
-          </a>
-        ))}
-      </div>
-      <div></div>
+      {tools.map((tool, index) => (
+        <a className="flex" href={tool.link} key={index} target="_blank">
+          <img
+            className="grayscale-0 sm:grayscale hover:grayscale-0"
+            src={tool.icon}
+            alt="icon"
+            key={index}
+            width={20}
+            height={20}
+          />
+        </a>
+      ))}
     </footer>
   );
 };
